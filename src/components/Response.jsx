a17d52591b7eba202fbd91f3600d83ece4b8af0e
hey-dola <dolamanirohidas696@gmail.com>
2024-08-19 20:51:16 +0530

    update

--- src/components/Response.jsx
+++ src/components/Response.jsx
@@ -4,8 +4,8 @@ import { SiGooglegemini } from "react-icons/si";
 const Response = ({ resp }) => {
 
     return (
-        <div className='answer max-w-[80%] self-start  rounded-md p-2 mb-4 flex gap-2'>
-            <div className="rounded-full border-[1px] h-7 w-7 flex items-center justify-center flex-shrink-0">
+        <div className='answer w-full lg:w-[80%] self-start  rounded-md p-2 mb-4 lg:flex gap-2'>
+            <div className="rounded-full border-[1px] h-7 w-7 flex items-center justify-center flex-shrink-0 mb-2 lg:mb-0">
                 <SiGooglegemini className='text-white' />
             </div>
             <p className='text-lg text-white/80 leading-tight font-light'>
